9f3cd9baa02a6326a33fbf8006e0f0be5a11b74a
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2022-12-19 12:59:55 -0300

    Improving database seed, changing synopsis data type

--- src/test/java/com/devsuperior/movieflix/controllers/UserControllerIT.java
+++ src/test/java/com/devsuperior/movieflix/controllers/UserControllerIT.java
@@ -1,10 +1,7 @@
 package com.devsuperior.movieflix.controllers;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.devsuperior.movieflix.tests.TokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,72 +12,73 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.movieflix.tests.TokenUtil;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 public class UserControllerIT {
 
-	@Autowired
-	private MockMvc mockMvc;
-	
-	@Autowired
-	private TokenUtil tokenUtil;
+    @Autowired
+    private MockMvc mockMvc;
 
-	private String visitorUsername;
-	private String visitorPassword;
-	private String memberUsername;
-	private String memberPassword;
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		
-		visitorUsername = "bob@gmail.com";
-		visitorPassword = "123456";
-		memberUsername = "ana@gmail.com";
-		memberPassword = "123456";
-	}
-	
-	@Test
-	public void getProfileShouldReturnSelfWhenVisitorLogged() throws Exception {
-		
-		String accessToken = tokenUtil.obtainAccessToken(mockMvc, visitorUsername, visitorPassword);
+    @Autowired
+    private TokenUtil tokenUtil;
 
-		ResultActions result =
-				mockMvc.perform(get("/users/profile")
-					.header("Authorization", "Bearer " + accessToken)
-					.accept(MediaType.APPLICATION_JSON));
-		
-		result.andExpect(status().isOk());
-		result.andExpect(jsonPath("$.id").isNotEmpty());
-		result.andExpect(jsonPath("$.name").isNotEmpty());
-		result.andExpect(jsonPath("$.email").value(visitorUsername));
-	}
+    private String visitorUsername;
+    private String visitorPassword;
+    private String memberUsername;
+    private String memberPassword;
 
-	@Test
-	public void getProfileShouldReturnSelfWhenMemberLogged() throws Exception {
-		
-		String accessToken = tokenUtil.obtainAccessToken(mockMvc, memberUsername, memberPassword);
+    @BeforeEach
+    void setUp() throws Exception {
+        memberUsername = "bob@gmail.com";
+        memberPassword = "123456";
+        visitorUsername = "ana@gmail.com";
+        visitorPassword = "123456";
+    }
 
-		ResultActions result =
-				mockMvc.perform(get("/users/profile")
-					.header("Authorization", "Bearer " + accessToken)
-					.accept(MediaType.APPLICATION_JSON));
-		
-		result.andExpect(status().isOk());
-		result.andExpect(jsonPath("$.id").isNotEmpty());
-		result.andExpect(jsonPath("$.name").isNotEmpty());
-		result.andExpect(jsonPath("$.email").value(memberUsername));
-	}
+    @Test
+    public void getProfileShouldReturnSelfWhenVisitorLogged() throws Exception {
 
-	@Test
-	public void getProfileShouldReturnUnauthorizedWhenNoUserLogged() throws Exception {
-		
-		ResultActions result =
-				mockMvc.perform(get("/users/profile")
-					.accept(MediaType.APPLICATION_JSON));
-		
-		result.andExpect(status().isUnauthorized());
-	}
+        String accessToken = tokenUtil.obtainAccessToken(mockMvc, visitorUsername, visitorPassword);
+
+        ResultActions result =
+                mockMvc.perform(get("/users/profile")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").isNotEmpty());
+        result.andExpect(jsonPath("$.name").isNotEmpty());
+        result.andExpect(jsonPath("$.email").value(visitorUsername));
+    }
+
+    @Test
+    public void getProfileShouldReturnSelfWhenMemberLogged() throws Exception {
+
+        String accessToken = tokenUtil.obtainAccessToken(mockMvc, memberUsername, memberPassword);
+
+        ResultActions result =
+                mockMvc.perform(get("/users/profile")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").isNotEmpty());
+        result.andExpect(jsonPath("$.name").isNotEmpty());
+        result.andExpect(jsonPath("$.email").value(memberUsername));
+    }
+
+    @Test
+    public void getProfileShouldReturnUnauthorizedWhenNoUserLogged() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(get("/users/profile")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isUnauthorized());
+    }
 }
